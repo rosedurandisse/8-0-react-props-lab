16e6ba58ae7b56a31b574d5b39669a56a2f871fe
Rose Durandisse <rosedurandisse@pursuit.org>
2021-11-03 19:10:16 -0400

    All Files

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,5 +1,11 @@
-const Progress = () => {
-  return null;
+const Progress = (props) => {
+  return (<section class="progress">
+    <h2>
+      Raised <span class="secondary">$335</span> of
+      <span class="secondary"> $ {props.max} </span>
+    </h2>
+  </section>
+  )
 };
 
 export default Progress;
--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,5 +1,16 @@
-const RecentDonations = () => {
-  return null;
+const RecentDonations = ({ name, amount, caption }) => {
+  return (
+    <ul>
+      <li>
+        <span>
+          {name} donated $ {amount}
+        </span>
+        {caption}
+
+      </li>
+
+    </ul>
+  );
 };
 
 export default RecentDonations;
